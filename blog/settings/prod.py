670ca179af1ace18f94b0c1b3d1e2041c9befab5
Jesus Flores <me@jesusfloressanjose.com>
2018-08-07 18:28:23 +0100

    Full path to db

--- blog/settings/prod.py
+++ blog/settings/prod.py
@@ -1,5 +1,14 @@
 from .base import *
 
 ALLOWED_HOSTS += ['blog.jesusfloressanjose.com']
+
 WSGI_APPLICATION = 'blog.wsgi-prod.application'
+
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/var/www/blog-django/db.sqlite3',
+    }
+}
