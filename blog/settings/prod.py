63c0bee997d3d370f081f3d9f85c8229b8932a69
Jesus Flores <me@jesusfloressanjose.com>
2018-08-07 18:45:32 +0100

    Remove full path to db

--- blog/settings/prod.py
+++ blog/settings/prod.py
@@ -5,10 +5,3 @@ ALLOWED_HOSTS += ['blog.jesusfloressanjose.com']
 WSGI_APPLICATION = 'blog.wsgi-prod.application'
 
 DEBUG = False
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/var/www/blog-django/db.sqlite3',
-    }
-}
