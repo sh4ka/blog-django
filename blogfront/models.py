05f8e2a2c5141ef8a4be2825b51a26e89bd3fcb3
Jesus Flores <me@jesusfloressanjose.com>
2018-09-16 16:18:45 +0100

    Add tags

--- blogfront/models.py
+++ blogfront/models.py
@@ -2,9 +2,17 @@ from django.db import models
 from ckeditor_uploader.fields import RichTextUploadingField
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=150)
     text = RichTextUploadingField()
+    tags = models.ManyToManyField(Tag)
     pub_date = models.DateTimeField('date published')
     published = models.BooleanField(default=False)
 
